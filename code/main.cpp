61996eb1a9fbc021ab536e674b54cd8f949146bd
HermesKeng <kaining1101@gmail.com>
2016-10-19 15:44:43 +0800

    Update main.cpp

--- code/main.cpp
+++ code/main.cpp
@@ -9,7 +9,7 @@
 #include "arduPiLoRa.h"
 #include <string>
 #include "FileTran.h"
-#include "rssi_to_distance/LoRa_RSSIToDistance.h"
+#include "LoRa_RSSIToDistance.h"
 using namespace std;
 
 int e;
